c08f44bc887ea7e2628a2ec69996c2de626a5351
Dentac-Lorb <edwardhunter@mail.adelphi.edu>
2026-09-02 16:40:24 -0400

    Water refraction.

---
+++ watersprite.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+@export var camera: Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	scale = camera.get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
