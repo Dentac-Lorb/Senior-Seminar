79ff79631d20a6d810722a6c2820c78fdcf14a22
Dentac-Lorb <edwardhunter@mail.adelphi.edu>
2026-09-11 15:22:51 -0400

    Optimize performance of fragment shader by skipping intensive calculations if fragment is too far away from fish.

--- fishshadersprite.gd
+++ fishshadersprite.gd
@@ -22,8 +22,22 @@ func _process(delta: float) -> void:
 	position = head.position
 	var positions = [headpos, (headpos + middlepos) / 2, middlepos, (middlepos + tailpos) / 2, tailpos, (tailpos + tail2pos) / 2, tail2pos]
 	var radii = [15, 20, 12, 10, 8, 5, 5]
+	var bounds = [INF,-INF,INF,-INF]
+	
+	for i in range(len(positions)):
+		if positions[i].x - radii[i] - 10 < bounds[0]:
+			bounds[0] = positions[i].x - radii[i] - 10
+		if positions[i].x + radii[i] + 10 > bounds[1]:
+			bounds[1] = positions[i].x + radii[i] + 10
+		if positions[i].y - radii[i] - 10 < bounds[2]:
+			bounds[2] = positions[i].y - radii[i] - 10
+		if positions[i].y + radii[i] + 10 > bounds[3]:
+			bounds[3] = positions[i].y + radii[i] + 10
+	
+	
 	#material.set_shader_parameter("pos1", headpos)
 	#material.set_shader_parameter("pos2", middlepos)
 	#material.set_shader_parameter("pos3", tailpos)
 	material.set_shader_parameter("positions", positions)
 	material.set_shader_parameter("radii", radii)
+	material.set_shader_parameter("bounds", bounds)
